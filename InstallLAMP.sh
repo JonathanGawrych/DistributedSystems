54b80ed5adfa31e00d81843285dd8e275d689f1d
Jonathan Gawrych <Jonathan.Gawrych@outlook.com>
2017-06-17 21:03:06 -0600

    Do not print phpinfo (recommended, although security through obscurity isn't a good practice)

--- InstallLAMP.sh
+++ InstallLAMP.sh
@@ -9,4 +9,4 @@ chown -R root:www /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www -type f -exec chmod 0664 {} +
-echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+#echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
