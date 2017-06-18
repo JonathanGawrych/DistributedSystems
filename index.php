77aab4e9f726eb878e4dd0ba449b4016674cf3e7
Jonathan Gawrych <Jonathan.Gawrych@outlook.com>
2017-06-17 21:15:55 -0600

    Add homepage

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lab 0: AWS Introduction</title>
+</head>
+<body>
+	<p>Name: Jonathan Gawrych</p>
+	<p>AMI ID: </p>
+	<p>User Data Script:</p>
+	<p><?php echo readfile("InstallLAMP.sh"); ?></p>
+</body>
+</html>
